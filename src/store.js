fa7283710e9c26704abf6a2de1e0bd16e98eae43
Dominic Ng <dominic.ng@coveinsurance.co.nz>
2019-05-19 22:24:48 +1200

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
 const initialState = {};
 
+const logger = createLogger({ collapsed: true });
+
 const middleware = [thunk];
+
+if (process.env.ENVIRONMENT !== 'production') {
+  middleware.push(logger);
+}
 
 const store = createStore(
   rootReducer,
